8308524d4af77a85ea96b98f257ce88b49c22020
Hosam91 <99843529+Hosam91@users.noreply.github.com>
2023-09-06 16:28:17 +0300

    add animations

--- src/OurProjects.jsx
+++ src/OurProjects.jsx
@@ -44,89 +44,42 @@ import b7 from './imgs/bkr/4_.webp'
 import b8 from './imgs/bkr/3_.webp'
 import b9 from './imgs/bkr/2_.webp'
 import b10 from './imgs/bkr/1_.webp'
+import { useEffect, useState } from 'react'
 
 // import h from './imgs/faces/11_.webp'
 
 // import s from './OurProjects.module.css'
 export default function OurProjects() {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Calculate the scroll threshold
+    const scrollThreshold = 1000;
+
+    // Check if the scroll position has reached the threshold
+    if (scrollPosition >= scrollThreshold) {
+      // Apply the animations for the columns
+      document.querySelectorAll('.hovering').forEach((column, index) => {
+        column.style.animationDelay = `${index * 0.5}s`;
+        column.classList.add('animate');
+      });
+    }
+  }, [scrollPosition]);
+  
   return (
-    // <>
-    //   <div className={s.myContainer}  id='projects'>
-    //   <div className={s.seven+s.h7}>
-    //       <img src={b4} alt="" />
-    //     </div>
-    //     <div className={s.virtical}>
-    //     <div className={''}>
-    //   <img src={b3} alt="" />
-
-    // </div>
-    //   <div className={'s.thirty'}>
-    //   <img src={b3} alt="" />
-
-    // </div>
-    //     </div>
-
-    //   </div>
-
-    //   <div className={s.myContainer}  >
-
-    //   <div className={s.thirty}>
-    //   <img src={b3} alt="" />
-    // </div>
-    //   <div className={s.seven}>
-    //   <img src={b2} alt="" />
-
-    // </div>
-
-    //   </div>
-
-    //     <div className={s.myContainer}>
-    //     <div className={s.fifty}>
-    //     <img src={b3} alt="" />
-
-    //     </div>
-    //     <div className={s.fifty}>
-    //     <img src={b2} alt="" />
-
-    //     </div>
-
-    //     </div>
-    //   <div className={s.myContainer}  >
-    //   <div className={s.seven}>
-    //       <img src={b2} alt="" />
-    // </div>
-    //   <div className={s.thirty}>
-    //   <img src={b3} alt="" />
-
-    // </div>
-
-    //   </div>
-
-    //   <div className={s.myContainer}  >
-
-    //   <div className={s.thirty}>
-    //   <img src={b3} alt="" />
-    // </div>
-    //   <div className={s.seven}>
-    //   <img src={b2} alt="" />
-
-    // </div>
-
-    //   </div>
-
-    //     <div className={s.myContainer}>
-    //     <div className={s.fifty}>
-    //     <img src={b2} alt="" />
-
-    //     </div>
-    //     <div className={s.fifty}>
-    //     <img src={b3} alt="" />
-
-    //     </div>
-
-    //     </div>
-
-    // </>
+   
     <>
       <div className="row gappy container-fluid">
         <div className="col-md-12">
@@ -134,145 +87,149 @@ export default function OurProjects() {
           <div className="underline"></div>
         </div>
 
-        
-        <div className="col-md-4 hovering ">
+        {/* {`column ${isVisible ? 'show' : ''}`} */}
+        <div className="col-md-4 hovering  left-column">
           <img src={b3} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+
+        <div className="col-md-4 hovering middle-column">
           <img src={b9} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+
+        <div className="col-md-4 hovering right-column ">
           <img src={b8} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+
+
+        <div className="col-md-4 hovering  left-column">
           <img src={b7} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering middle-column">
           <img src={b6} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering right-column">
           <img src={b5} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering  left-column">
           <img src={b44} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering middle-column">
           <img src={b3} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering right-column">
           <img src={b2} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering  left-column">
           <img src={b1} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering middle-column">
           <img src={b} alt="" />
         </div>
-        <div className="col-md-4 hovering ">
+        <div className="col-md-4 hovering right-column">
           <img src={c} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={d} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={e} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={f} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={g} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={h} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={b4} alt="" />
         </div>
 
      
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={k} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={l} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={m} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={o} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={p} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={q} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={r} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={s} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={t} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={w} alt="" />
         </div>
         {/* <div className="col-md-4 hovering">
           <img src={v} alt="" />
         </div> */}
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={u} alt="" />
         </div>
         <div className="col-md-4 hovering">
           <img src={x} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={y} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={z} alt="" />
         </div>
         <div className="col-md-4 hovering">
           <img src={a1} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={a2} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={a3} alt="" />
         </div>
         <div className="col-md-4 hovering">
           <img src={a4} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={a5} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={a6} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={a7} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering  left-column">
           <img src={a8} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={a9} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={h1} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering left-column">
           <img src={h2} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering middle-column">
           <img src={h3} alt="" />
         </div>
-        <div className="col-md-4 hovering">
+        <div className="col-md-4 hovering right-column">
           <img src={h4} alt="" />
         </div>
       </div>
